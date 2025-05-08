ee510faebae7f64ebc8797b69d68fd2e0052479a
Nodal-dot <zeroxhero@yandex.ru>
2025-05-08 12:49:12 +0300

    feat: add theme provider

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -29,12 +29,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from './providers/ThemeProvider';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container);
 root.render(
+
     <React.StrictMode>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
-);
+);1
